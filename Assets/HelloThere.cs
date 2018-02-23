52ab557335df53f44f9222c09c1a27386ab0b14e
Joseph Bluck <s4909501@PC42868>
2018-02-23 17:30:22 +0000

    Testing GUI
    Signed-off-by: Joseph Bluck <s4909501@PC42868>

--- Assets/HelloThere.cs
+++ Assets/HelloThere.cs
@@ -6,7 +6,7 @@ public class HelloThere : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log("General Kenobi!");
+        Debug.Log("Generaenobi!");
 	}
 	
 	// Update is called once per frame
