abea75f1ac695cd746b31ebdafd7d757bde462a8
Jamie Agate <i7469179@PC40577>
2018-02-23 18:55:37 +0000

    added basic machine code

---
+++ Assets/Machine/Machine.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Machine : MonoBehaviour {
+    E_Status currentStatus;
+    enum E_Status {e_working, e_broken };
+	// Use this for initialization
+	void Start () {
+        currentStatus = E_Status.e_working;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch(currentStatus)
+        {
+            case E_Status.e_working:
+
+                break;
+            case E_Status.e_broken:
+
+                break;
+        }
+	}
+
+    void Working()
+    {
+
+    }
+
+    void Broken()
+    {
+
+    }
+
+}
+
+
