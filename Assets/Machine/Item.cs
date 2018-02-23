d8e52380eb1e21135f3380a36bcc373042ab84c6
Joseph Bluck <s4909501@PC42868>
2018-02-23 22:13:52 +0000

    Basic machine and item functionality

---
+++ Assets/Machine/Item.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    enum E_Step { s_gbProc, s_gbCase, s_dsProc, s_dsCase, s_switchProc, s_switchCase, s_colour };
+    enum E_Type { t_gb, t_ds, t_switch };
+
+    E_Step nextStep;
+
+    void Awake()
+    {
+        nextStep = E_Step.s_gbProc;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        // 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public int GetStep()
+    {
+        return (int)nextStep;
+    }
+
+    public void SetType(int _type)
+    {
+        switch (_type)
+        {
+            case (int)E_Type.t_gb:
+                nextStep = E_Step.s_gbProc;
+                break;
+            case (int)E_Type.t_ds:
+                nextStep = E_Step.s_dsProc;
+                break;
+            case (int)E_Type.t_switch:
+                nextStep = E_Step.s_switchProc;
+                break;
+        }
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        GameObject collider = other.gameObject;
+
+        if (collider.GetComponent<PlaceholderPlayer>() != null)
+        {
+            if (collider.GetComponent<PlaceholderPlayer>().GetItem() == null) // The player isn't carrying anything
+            {
+                collider.GetComponent<PlaceholderPlayer>().CanInteract("Pick Up");
+
+                if(collider.GetComponent<PlaceholderPlayer>().IsInteracting())
+                {
+                    collider.GetComponent<PlaceholderPlayer>().SetItem(gameObject);
+                }
+            }
+        }
+    }
+}
--- Assets/Machine/Machine.cs
+++ Assets/Machine/Machine.cs
@@ -2,9 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Base class for machine
+
 public class Machine : MonoBehaviour {
-    E_Status currentStatus;
     enum E_Status {e_working, e_broken };
+    public enum E_Type { t_gbProc, t_gbCase, t_dsProc, t_dsCase, t_switchProc, t_switchCase, t_colour };
+
+    E_Status currentStatus;
+    public E_Type currType;
+    
+    private bool canInteract = false;
+
+
+
 	// Use this for initialization
 	void Start () {
         currentStatus = E_Status.e_working;
@@ -15,24 +25,52 @@ public class Machine : MonoBehaviour {
 		switch(currentStatus)
         {
             case E_Status.e_working:
-
+                Working();
                 break;
             case E_Status.e_broken:
-
+                Broken();
                 break;
         }
+        
 	}
 
     void Working()
     {
-
+        // Do working stuff
     }
 
     void Broken()
     {
+        // Do broken shit
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        GameObject collider = other.gameObject;
+
+        if (collider.GetComponent<PlaceholderPlayer>() != null)
+        {
+            // Debug.Log("Player can interact!");
+            canInteract = true;
+
+            if (collider.GetComponent<PlaceholderPlayer>().GetNextStep() == (int)currType)
+            {
+                collider.GetComponent<PlaceholderPlayer>().CanInteract("Interact");
+            }
+        }
+        else
+        {
+            canInteract = false;
+        }
+    }
+
+    void Interact(GameObject _player, GameObject _item)
+    {
 
     }
 
+
+    
 }
 
 
