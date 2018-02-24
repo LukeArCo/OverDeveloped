1e9519ad4ac9799c2aa0b51d5a3290ef47681333
LukeArCo <lukeac1997@hotmail.co.uk>
2018-02-24 15:57:55 +0000

    mergepls

--- Assets/Machine/Item.cs
+++ Assets/Machine/Item.cs
@@ -13,7 +13,6 @@ public class Item : MonoBehaviour
     public E_Step m_nextStep;
 >>>>>>> b47cf68ecd9d7ab151e95fe5674cf686f7d44169
 
-    bool isProcessing = false;
     bool m_isProcessing = false;
     float m_paintTimer;
 
@@ -31,26 +30,20 @@ public class Item : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         if(m_nextStep == E_Step.s_complete && m_isProcessing && m_paintTimer > 0) { m_paintTimer -= Time.deltaTime; }
     }
 
     public int GetStep()
     {
-        return (int)nextStep;
         return (int)m_nextStep;
     }
 
     public void AdvanceStep()
     {
-        if((int)nextStep % 2 == 0 && (int)nextStep < 6)
         if((int)m_nextStep % 2 == 0 && (int)m_nextStep < 6)
         {
-            nextStep++;
             m_nextStep++;
         }
-        else if ((int)nextStep < 6) { nextStep = E_Step.s_colour; }
-        else { nextStep = E_Step.s_complete; }
         else if ((int)m_nextStep < 6) { m_nextStep = E_Step.s_colour; }
         else { m_nextStep = E_Step.s_complete; Debug.Log("IS COMPLETE!"); }
     }
@@ -60,15 +53,12 @@ public class Item : MonoBehaviour
         switch (_type)
         {
             case (int)E_Type.t_gb:
-                nextStep = E_Step.s_gbProc;
                 m_nextStep = E_Step.s_gbProc;
                 break;
             case (int)E_Type.t_ds:
-                nextStep = E_Step.s_dsProc;
                 m_nextStep = E_Step.s_dsProc;
                 break;
             case (int)E_Type.t_switch:
-                nextStep = E_Step.s_switchProc;
                 m_nextStep = E_Step.s_switchProc;
                 break;
         }
@@ -82,7 +72,6 @@ public class Item : MonoBehaviour
         if (collider.GetComponent<PlayerController>() != null)
         {
 
-            if (collider.GetComponent<PlayerController>().GetItem() == null && !isProcessing) // The player isn't carrying anything
             if (collider.GetComponent<PlayerController>().GetItem() == null && !m_isProcessing) // The player isn't carrying anything
             {
                 collider.GetComponent<PlayerController>().CanInteract("Pick Up"); // Pickup indicator
